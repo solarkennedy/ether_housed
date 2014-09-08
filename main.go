5136c923322af73f7e03cb9b37351e4554f10ac7
Kyle Anderson <kyle@xkyle.com>
2014-09-07 18:26:23 -0700

    Added default port and log behavior

--- main.go
+++ main.go
@@ -4,22 +4,38 @@ import (
     "fmt"
     "net/http"
     "os"
+    "log"
+    "log/syslog"
 )
 
+func setup_logging() {
+    logwriter, e := syslog.New(syslog.LOG_NOTICE, "ether_housed")
+    if e == nil {
+        log.SetOutput(logwriter)
+    }
+}
+
 func main() {
-    http.HandleFunc("/", hello)
+    http.HandleFunc("/", usage)
+    http.HandleFunc("/on", usage)
+    http.HandleFunc("/off", usage)
+    http.HandleFunc("/state", usage)
+    http.HandleFunc("/target_mac", usage)
+
     port := os.Getenv("PORT")
     if port == ""{
-        panic("No PORT variable. Please set one")
+        port = "3000"
+        log.Print("No PORT variable. Defaulting to 3000")
     }
-    fmt.Println("listening on " + port + "..." )
+    log.Print("listening on " + port + "..." )
     err := http.ListenAndServe(":"+port, nil)
     if err != nil {
       panic(err)
     }
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(res, "hello, world")
-    fmt.Println("200: hello, world")
+func usage(res http.ResponseWriter, req *http.Request) {
+    msg := "Welcome to ether_house."
+    fmt.Fprintln(res, msg)
+    fmt.Println("200: " + msg)
 }
