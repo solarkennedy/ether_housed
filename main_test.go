cf76972e69fa32108d0f447cb114c1811625a51b
Kyle Anderson <kyle@xkyle.com>
2014-09-28 11:33:52 -0700

    Added more boolarraytoin test cases

--- main_test.go
+++ main_test.go
@@ -5,9 +5,25 @@ import "testing"
 func TestMain(t *testing.T) {
 }
 
-func TestBoolarraytoint(t *testing.T) {
+func TestBoolarraytoint240(t *testing.T) {
 	input := []bool{false, false, false, false, true, true, true, true}
 	expected := int64(240)
+	actual := boolarraytoint(input)
+	if expected != actual {
+		t.Error("Expected, actual: ", expected, actual)
+	}
+}
+func TestBoolarraytoint0(t *testing.T) {
+	input := []bool{false, false, false, false, false, false, false, false}
+	expected := int64(0)
+	actual := boolarraytoint(input)
+	if expected != actual {
+		t.Error("Expected, actual: ", expected, actual)
+	}
+}
+func TestBoolarraytoint255(t *testing.T) {
+	input := []bool{true, true, true, true, true, true, true, true}
+	expected := int64(255)
 	actual := boolarraytoint(input)
 	if expected != actual {
 		t.Error("Expected, actual: ", expected, actual)
