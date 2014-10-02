ef08b0eef3def460a1e663d5df23c6def54de752
Kyle Anderson <kyle@xkyle.com>
2014-10-01 22:03:30 -0700

    More godoc attempts

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// Currently everything for ether_housed is in the main package
 package main
 
 import (
@@ -16,6 +17,7 @@ import (
 
 const NUM_HOUSES = 8
 
+// common is a struct to store the global state and config
 type common struct {
 	lock       sync.RWMutex
 	state      []bool
@@ -34,22 +36,23 @@ func (c *common) Get(id int) bool {
 	return d
 }
 
+// Set will lock and set the state of a house
 func (c *common) Set(id int, d bool) {
 	c.lock.Lock()
 	defer c.lock.Unlock()
 	c.state[id] = d
 }
 
-
+// Gets state from an external datastore
 func load_existing_state() {
-	// TODO: Get state out of memcache
+	// BUG: Get state out of memcache
 	Common.state = []bool{false, false, false, false, true, true, true, true}
 	return
 }
 
+// We get the config for which MAC addresses are associated with each house
+// from the environment
 func load_target_macs() {
-	// We get the config for which MAC addresses are associated with each house
-	// From the environment
 	Common.target_mac = []string{"", "", "", "", "", "", "", ""}
 	for i := 0; i < NUM_HOUSES; i++ {
 		Common.target_mac[i] = strings.TrimSpace(os.Getenv("MAC" + strconv.Itoa(i)))
@@ -60,6 +63,7 @@ func load_target_macs() {
 	}
 }
 
+// load_api_keys reads in an APIKEYN environment variable for each houseid
 func load_api_keys() {
 	Common.api_key = []string{"", "", "", "", "", "", "", ""}
 	for i := 0; i < NUM_HOUSES; i++ {
@@ -113,8 +117,8 @@ func usage(res http.ResponseWriter, req *http.Request) {
 	log.Println("200: " + req.URL.Path)
 }
 
+// Convert our array of booleans into a binary representation for http output
 func boolarraytoint(bool_array []bool) (the_int int64) {
-	// Convert our array of booleans into a binary representation for http output
 	for index, value := range bool_array {
 		if value == true {
 			the_int += int64(math.Exp2(float64(index)))
@@ -170,7 +174,7 @@ func target_mac_handler(res http.ResponseWriter, req *http.Request) {
 		target_mac_binary := mactobinary(target_mac)
 		target_mac_string := string(target_mac_binary[:6])
 		fmt.Fprintf(res, target_mac_string)
-		log.Printf("200: target_mac: ", target_mac)
+		log.Printf("200: target_mac: %v ", target_mac)
 	} else {
 		http.Error(res, "403 Forbidden : you can't access this resource.", 403)
 		log.Printf("403: /state from %v, using api key %v", house_id, api_key)
@@ -213,6 +217,7 @@ func turn_off(res http.ResponseWriter, req *http.Request) {
 	}
 }
 
+// validate_key ensures that the provided key matches the one stored for that house_id
 func validate_key(api_key string, house_id int) bool {
 	return Common.api_key[house_id] == api_key
 }
