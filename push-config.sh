04b40b403d2c976796ce7614c93d22f1846d5e3d
Kyle Anderson <kyle@xkyle.com>
2014-12-24 12:25:25 -0800

    Ignore comments in config

--- push-config.sh
+++ push-config.sh
@@ -2,6 +2,6 @@
 set -eu
 IFS="
 "
-for CONFIG in `grep '=' secrets.sh | sed 's/export //g'`; do
+for CONFIG in `grep '=' secrets.sh | grep -v '^#'| sed 's/export //g'`; do
   heroku config:set $CONFIG
 done
