d0daafe32eef676ee784cda8e6f7efdf983f4e4c
Kyle Anderson <kyle@xkyle.com>
2014-12-29 19:23:33 -0800

    Fix 24hour report

--- tools/24h_report.sh
+++ tools/24h_report.sh
@@ -1,6 +1,6 @@
 EVENTS=`papertrail --min-time "24 hours ago" "program:app/web.1 AND (turn_off OR turn_on)"`
 
-for HOUSE in {0..8} ; do
+for HOUSE in {0..7} ; do
 	echo "Usage report for etherhouse$HOUSE:"
 	echo "$EVENTS" | egrep ": ${HOUSE} $"
 	echo
